a35d58c6d5e9e6687d66263b5d066f0a6d89b08a
Opilane TTHK <opilane@tthk.ee>
2026-01-15 17:37:29 +0200

    Tunnitöö

--- OMISTAMINE_ANDMETÜÜBID_SISEND_VÄLJUND_MUUTUJA/MATH & INPUT/MathInput1/Program.cs
+++ OMISTAMINE_ANDMETÜÜBID_SISEND_VÄLJUND_MUUTUJA/MATH & INPUT/MathInput1/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace MathInput1
+﻿internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        /*
+        * -------------
+         * MathInput1-1
+         * -------------
+        */
+
+        /*
+         int vanus = 0;
+         Console.Write("Sisesta oma vanus: ");
+         vanus = int.Parse(Console.ReadLine());
+
+         int aastaarv = 0;
+         Console.Write("Sisesta käesolev kalendriaasta: ");
+         aastaarv = int.Parse(Console.ReadLine());
+
+         int synniaasta = aastaarv - vanus;
+         //Console.WriteLine(synniaasta);
+        */
+
+        /*
+         * -------------
+         * MathInput1-2
+         * -------------
+        */
+
+        /*
+        string nimi = String.Empty;
+        Console.Write("Palun sisesta oma nimi: ");
+        nimi = Console.ReadLine();
+
+        Console.WriteLine($"Tervist {nimi}!");
+        */
+
+
+        /*
+         * -------------
+         * MathInput1-3
+         * -------------
+        */
+
+        float kommiKaal = 0;
+        int sopradeKogus = 0;
+
+        Console.Write("Mitu grammi kommi sul on: ");
+        kommiKaal = float.Parse(Console.ReadLine());
+
+        Console.Write("Mitu sõpra sul on: ");
+        sopradeKogus = int.Parse(Console.ReadLine());
+        Console.WriteLine(kommiKaal / sopradeKogus + "g");
+
+        // MathInput1-4
+        // MathInput1-5
     }
 }
