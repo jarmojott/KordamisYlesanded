01b498826b4115fde09a7a26fcadd01a0eded55d
Opilane TTHK <opilane@tthk.ee>
2026-01-15 16:28:41 +0200

    Lisasin uue repor koos uue teemaga

---
+++ OMISTAMINE_ANDMETÜÜBID_SISEND_VÄLJUND_MUUTUJA/MATH & INPUT/MathInput1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MathInput1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
