5724ae82039549801314a941b6516f73f33a7220
Opilane TTHK <opilane@tthk.ee>
2026-02-26 15:53:55 +0200

    Meetodite harjutamine

---
+++ MEETODID/1-Tänavanimed/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Threading.Channels;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Sisesta tänava nimi.");
+        //Console.WriteLine(inputAndCapitalize());
+
+        List<string> nimed = new List<string>();
+        Console.WriteLine("Kirjuta siia oma kodukandi tänavanimed. Lõpetamiseks kirjuta 'stop'");
+        String sisestus = string.Empty;
+        do
+        {
+            Console.WriteLine("Sisesta tänava nimi.");
+            sisestus = inputAndCapitalize();
+
+            if (sisestus.ToLower() != "stop")
+                nimed.Add(sisestus);
+        }
+        while (sisestus.ToLower() != "stop");
+
+        //Console.WriteLine(string.Join(nimed);
+
+        Console.WriteLine("Sisesta ebameeldiv täht.");
+        string täht = inputAndCapitalize(" ");
+        List<string> filtreeritud = filtreeriAndmed(nimed, täht);
+        Console.WriteLine("Filtreeritud tänava nimed:");
+        kuvaAndmed(filtreeritud);
+    }
+
+    private static void kuvaAndmed(List<string> kuvatavadAndmed)
+    {
+        if (kuvatavadAndmed.Count > 0)
+        {
+            for (int i = 0; i < kuvatavadAndmed.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + kuvatavadAndmed.ElementAt(i));
+            }
+        } else
+        {
+
+            Console.WriteLine("Andmeid pole!");
+        }
+    }
+
+    private static List<string> filtreeriAndmed(List<string> nimed, string täht)
+    {
+        int loendur = 0;
+        List<string> filtreeritud = new List<string>();
+        foreach (string nimi in nimed)
+        {
+            if (!nimi.ToLower().Contains(täht.Trim().ToLower()))
+                filtreeritud.Add(nimi);
+            else
+                loendur++;
+        }
+        Console.WriteLine("Eemaldati "+loendur+".");
+        return filtreeritud;
+    }
+
+    public static string inputAndCapitalize()
+    {
+        string sisestus = String.Empty;
+        do
+        {
+            Console.Write("Sinu vastus: ");
+            sisestus = Console.ReadLine();
+
+            if (sisestus.Length == 1)
+                Console.WriteLine("Tänava nimi peab olema pikem kui 1 sümbol.");
+        }
+
+        while (string.IsNullOrEmpty(sisestus) || sisestus.Length == 1);
+        return sisestus.Substring(0, 1).ToUpper() + sisestus.Substring(1).ToLower();
+    }
+
+    public static string inputAndCapitalize(string addition)
+    {
+        string sisestus = String.Empty;
+        do
+        {
+            Console.Write("Sinu vastus: ");
+            sisestus = Console.ReadLine().Trim();
+
+            if (sisestus.Length > 1)
+                Console.WriteLine("Sisesta ainult 1 täht!");
+        }
+
+        while (string.IsNullOrEmpty(sisestus) || sisestus.Length > 1);
+        return sisestus;
+    }
+}
---
+++ MEETODID/2-Sulgudega kalkulaator/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        /* Kirjuta, koos sisendikontrolliga ja sisesndi normaliseerimisega programm:
+         * küsib kasutajalt 3 tehet, esimene ja kolmas peaksid olema sulgude vahel.
+         * kasutaja saab määrata igale tehtele märgi.
+         * 
+         */
+
+        string[] tehteRida = new string[7];
+        Console.WriteLine("### Sulgudega kalkulaator ###");
+        Console.WriteLine("(arv tehe arv ) tehe (arv tehe arv)");
+
+        for (int i = 0; i < tehteRida.Length; i++)
+        {
+            string sisestus = String.Empty;
+            do
+            {
+                if (i % 2 == 0)
+                {
+                    do
+                    {
+                        Console.WriteLine($"Palun sisesta {i + 1} element milleks on arv:");
+                        sisestus = Console.ReadLine();
+
+                    } while (!double.TryParse(sisestus, out _));
+                }
+                else
+                {
+                    do
+                    {
+                        Console.WriteLine($"Palun sisesta {i + 1} element milleks on tehe:");
+                        sisestus = Console.ReadLine();
+                    }
+                    while (sisestus != "+" && sisestus != "-" && sisestus != "*" && sisestus != "/");
+                }
+
+            }
+            while (sisestus == String.Empty);
+
+            tehteRida[i] = sisestus;
+        }
+
+        double tehe = üksTehe(
+            [
+                üksTehe(
+                [tehteRida[0], tehteRida[1], tehteRida[2]]).ToString(),tehteRida[3],
+                üksTehe(
+                    [tehteRida[4], tehteRida[5], tehteRida[6]]).ToString()
+            ]
+            );
+        Console.WriteLine("Vastus on: " + tehe);
+    }
+
+    private static double üksTehe(string[] tehteRida)
+    {
+
+        switch (tehteRida[1])
+        {
+            case "+":
+                return liitmine(double.Parse(tehteRida[0]), double.Parse(tehteRida[2]));
+            case "-":
+                return lahutamine(double.Parse(tehteRida[0]), double.Parse(tehteRida[2]));
+            case "*":
+                return korrutamine(double.Parse(tehteRida[0]), double.Parse(tehteRida[2]));
+            case "/":
+                return jagamine(double.Parse(tehteRida[0]), double.Parse(tehteRida[2]));
+            default:
+                return 0;
+        }
+    }
+
+    public static double liitmine(double arv1, double arv2)
+    {
+
+        return arv1 + arv2;
+    }
+
+    public static double lahutamine(double arv1, double arv2)
+    {
+
+        return arv1 - arv2;
+    }
+
+    public static double korrutamine(double arv1, double arv2)
+    {
+
+        return arv1 * arv2;
+    }
+
+    public static double jagamine(double arv1, double arv2)
+    {
+
+        return arv1 / arv2;
+    }
+}
