ebc9f547e77d81804ce894f2daa6658f641139b7
Opilane TTHK <opilane@tthk.ee>
2026-03-12 15:05:05 +0200

    Harjutus: Adventure

---
+++ CLASS_ENUM_STRUCT/Adventure/Player.cs
@@ -0,0 +1,42 @@
+﻿namespace Adventure
+{
+    public enum BackPackItemType
+    {
+        Nuga, Kilp, Vibu
+    }
+
+    public struct LocationType
+    {
+        public int X;
+        public int Y;
+
+        public LocationType(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Player
+    {
+        int Health = 100;
+        int Lives = 3;
+        int Money = 0;
+        List<BackPackItemType> Items = new List<BackPackItemType>();
+        LocationType Location;
+
+        public Player(int playerHealth, int playerLives, int playerMoney, List<BackPackItemType> playerBackpack, LocationType playerLocation)
+        {
+            Health = playerHealth;
+            Lives = playerLives;
+            Money = playerMoney;
+            Items = playerBackpack;
+            Location = playerLocation;
+        }
+
+        public override string ToString()
+        {
+            return $"Mängijal on {Lives} elu, {Health} HP ja {Money} raha.\nTema kordinaadid on ({Location.X}, {Location.Y})\nSeljakotis on: "+ string.Join(",", Items);
+        }
+    }
+}
---
+++ CLASS_ENUM_STRUCT/Adventure/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Adventure
+{
+    internal class Program
+    {
+        /*
+         * 1: Tee Player klass, koos viie andmeväljaga.
+         * Player klassis on üks konstruktur kus kasutakse kõiki andmeid.
+         * Andmeväljad on: Lives, Health, struct Location (X ja Y), Backpack, Money
+         * Vaikeväärtused on Lives=3 ja Health (100)
+         */
+
+        static void Main(string[] args)
+        {
+            Player newPlayer = new Player(50, 5, 10, new List<BackPackItemType>() { BackPackItemType.Vibu }, new LocationType(99, 88));
+            Console.WriteLine(newPlayer.ToString());
+        }
+    }
+}
